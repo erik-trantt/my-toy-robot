e2846051ad6ba5573a5de1299338ac3690ab4d2d
Erik Tran <erik.tran.tt@gmail.com>
2020-07-24 18:28:18 +1000

    feat: implemented tests for ReportCommand class

--- test/test_report_command.rb
+++ test/test_report_command.rb
@@ -4,7 +4,30 @@ require_relative '../app/models/robot'
 require_relative '../app/models/position'
 
 describe ReportCommand, '#execute' do
-  it '' do
+  before do
+    @robot = Robot.new
+    @table = Table.new(5, 5)
+  end
 
+  it 'return position as text with a VALID position' do
+    position = Position.new(1, 3, 'NORTH')
+    place_command = PlaceCommand.new(@table, @robot, position)
+    place_command.execute
+
+    report_command = ReportCommand.new(@robot)
+    expected = "1,3,NORTH\n"
+
+    _{report_command.execute}.must_output expected
+  end
+
+  it 'return "Not in place" with an INVALID position' do
+    invalid_position = Position.new(1, -3, 'NORTH')
+    place_command = PlaceCommand.new(@table, @robot, invalid_position)
+    place_command.execute
+
+    report_command = ReportCommand.new(@robot)
+    expected = "Not in place\n"
+
+    _{report_command.execute}.must_output expected
   end
 end
