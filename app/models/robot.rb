3259647a3a7d2c5ed545cdc841d4b7a27841fa54
Erik Tran <erik.tran.tt@gmail.com>
2020-07-24 16:08:07 +1000

    feat: implemented Robot, removed dependencies on Table model from prev approach

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,86 +1,18 @@
 # class Robot
 class Robot
-  attr_reader :errors, :pos_x, :pos_y, :facing
+  attr_accessor :position
 
-  def initialize(table, attributes = {})
-    @table = table
-    place(attributes)
+  def initialize(position = nil)
+    @position = position
   end
 
-  # def place(attributes)
-  #    Assign position (x,y) and facing direction for the robot
-  #    Assign all instance variables to +nil+ if no information is provided
-  def place(attributes)
-    @pos_x = attributes[:pos_x] || nil
-    @pos_y = attributes[:pos_y] || nil
-    @facing = attributes[:facing] || nil
+  def placed?
+    !@position.nil?
   end
 
-  # def report_position
-  #    Return the robot's position and facing direction in format of "X,Y,Direction"
-  #    Return "Not in place" if the robot has not been placed on table
   def report_position
     return 'Not in place' unless placed?
 
-    "#{@pos_x},#{@pos_y},#{@facing}"
-  end
-
-  # def placed?
-  #   Check if the robot knows its position on the table and facing direction
-  def placed?
-    !(@pos_x && @pos_y && @facing).nil?
-  end
-
-  def move
-    case @facing
-    when @table.directions[:north] then move_north
-    when @table.directions[:east] then move_east
-    when @table.directions[:south] then move_south
-    when @table.directions[:west] then move_west
-    end
-  end
-
-  def turn_right
-    turn
-  end
-
-  def turn_left
-    clockwise = false
-    turn(clockwise)
-  end
-
-  def attributes
-    {
-      pos_x: @pos_x,
-      pos_y: @pos_y,
-      facing: @facing
-    }
-  end
-
-  private
-
-  def turn(clockwise = true)
-    modifier = 1
-    modifier *= -1 unless clockwise == true
-    directions = @table.directions.values
-    original_direction_value = directions.index(@facing)
-    new_direction_value = (original_direction_value + modifier) % 4
-    @facing = directions[new_direction_value]
-  end
-
-  def move_north
-    @pos_y += 1
-  end
-
-  def move_south
-    @pos_y -= 1
-  end
-
-  def move_west
-    @pos_x -= 1
-  end
-
-  def move_east
-    @pos_x += 1
+    @position.to_h.values.join(',')
   end
 end
