70b494500d4c4279e8eec8332c45546cd0bce61b
Erik Tran <erik.tran.tt@gmail.com>
2020-07-26 01:05:33 +1000

    move unit test files into /unit folder, updated Rakefile accordingly

--- test/unit/test_command_factory.rb
+++ test/unit/test_command_factory.rb
@@ -1,12 +1,12 @@
 require 'minitest/autorun'
-require_relative '../app/factories/command_factory'
-require_relative '../app/models/place_command'
-require_relative '../app/models/move_command'
-require_relative '../app/models/left_command'
-require_relative '../app/models/right_command'
-require_relative '../app/models/report_command'
-require_relative '../app/models/table'
-require_relative '../app/models/robot'
+require_relative '../../app/factories/command_factory'
+require_relative '../../app/models/place_command'
+require_relative '../../app/models/move_command'
+require_relative '../../app/models/left_command'
+require_relative '../../app/models/right_command'
+require_relative '../../app/models/report_command'
+require_relative '../../app/models/table'
+require_relative '../../app/models/robot'
 
 describe CommandFactory, '#parse' do
 
--- test/unit/test_place_command.rb
+++ test/unit/test_place_command.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
-require_relative '../app/models/place_command'
-require_relative '../app/models/table'
-require_relative '../app/models/robot'
-require_relative '../app/models/position'
+require_relative '../../app/models/place_command'
+require_relative '../../app/models/table'
+require_relative '../../app/models/robot'
+require_relative '../../app/models/position'
 
 describe PlaceCommand, '#execute' do
   before do
