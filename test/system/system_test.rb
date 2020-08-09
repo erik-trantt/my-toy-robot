275aca0919c4b5d5612fa9cdd7ea9d0a9b4b28eb
Erik <erik.tran.tt@gmail.com>
2020-08-09 18:12:08 +1000

    refactor: removed internal describe blocks, each example is an it block

--- test/system/system_test.rb
+++ test/system/system_test.rb
@@ -17,65 +17,47 @@ describe "System Test" do
 
   let (:inputs) { FileHelper.readlines_to_array(@input_file) }
   let (:actual) { OutputHelper.capture_output(@factory, inputs) }
-
-  describe 'Example #a' do
-    
-    it 'return "0,1,NORTH\n"' do
-      @input_file = 'test/data/test_data_example_a.txt'
-      expected = "0,1,NORTH\n"
-
-      _(actual).must_equal expected
-    end
-  end
-
-  describe 'Example #b' do
-    
-    it 'return "0,0,WEST\n"' do
-      @input_file = 'test/data/test_data_example_b.txt'
-      expected = "0,0,WEST\n"
-
-      _(actual).must_equal expected
-    end
-  end
-
-  describe 'Example #c' do
-    
-    it 'return "3,3,NORTH\n"' do
-      @input_file = 'test/data/test_data_example_c.txt'
-      expected = "3,3,NORTH\n"
-
-      _(actual).must_equal expected
-    end
-  end
-
-  describe 'Example #d' do
-    
-    it 'from "0,0,NORTH" move 5 times then return "0,4,NORTH\n"' do
-      @input_file = 'test/data/test_data_example_d.txt'
-      expected = "0,4,NORTH\n"
-
-      _(actual).must_equal expected
-    end
-  end
-
-  describe 'Example #e' do
-    
-    it 'from "0,0,EAST" move 2 times, turn left, move 4 times, then return "2,4,NORTH\n"' do
-      @input_file = 'test/data/test_data_example_e.txt'
-      expected = "2,4,NORTH\n"
-
-      _(actual).must_equal expected
-    end
-  end
-
-  describe 'Example #f' do
-    
-    it 'from "2,2,SOUTH" move 3 times, turn right, move 3 times, then return "0,0,WEST\n"' do
-      @input_file = 'test/data/test_data_example_f.txt'
-      expected = "2,0,SOUTH\n0,0,WEST\n"
-
-      _(actual).must_equal expected
-    end
-  end
   
+  it 'A. return "0,1,NORTH\n"' do
+    @input_file = 'test/data/test_data_example_a.txt'
+    expected = "0,1,NORTH\n"
+
+    _(actual).must_equal expected
+  end
+
+  it 'B. return "0,0,WEST\n"' do
+    @input_file = 'test/data/test_data_example_b.txt'
+    expected = "0,0,WEST\n"
+
+    _(actual).must_equal expected
+  end
+
+  it 'C. return "3,3,NORTH\n"' do
+    @input_file = 'test/data/test_data_example_c.txt'
+    expected = "3,3,NORTH\n"
+
+    _(actual).must_equal expected
+  end
+
+  it 'D. return "0,4,NORTH\n", from "0,0,NORTH" move 5 times' do
+    @input_file = 'test/data/test_data_example_d.txt'
+    expected = "0,4,NORTH\n"
+
+    _(actual).must_equal expected
+  end
+
+  it 'E. return "2,4,NORTH\n", from "0,0,EAST" move 2 times, turn left, move 4 times' do
+    @input_file = 'test/data/test_data_example_e.txt'
+    expected = "2,4,NORTH\n"
+
+    _(actual).must_equal expected
+  end
+
+  it 'F. return "0,0,WEST\n", from "2,2,SOUTH" move 3 times, turn right, move 3 times' do
+    @input_file = 'test/data/test_data_example_f.txt'
+    expected = "2,0,SOUTH\n0,0,WEST\n"
+
+    _(actual).must_equal expected
+  end
+
 end
